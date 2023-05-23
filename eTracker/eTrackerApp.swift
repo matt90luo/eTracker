1487922209b4165a654664afc4ddc1ed2d7c24cb
ukiml <matt90luo@hotmail.com>
2023-05-24 07:22:48 +0800

    Initial Commit

---
+++ eTracker/eTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  eTrackerApp.swift
+//  eTracker
+//
+//  Created by Ma Luo on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct eTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
